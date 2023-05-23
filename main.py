9d4430dcb74ecc2b08b74bb401d7bdc8db8f8fac
LeVassalLouis <134364339+LeVassalLouis@users.noreply.github.com>
2023-05-23 09:27:52 +0200

    changement du menu pour display_quotes

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from functions import *
 def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
-    print("2. All quotes")
-    print("3. Add quote")
-    print("4. Exit")
+    print("2. Display quotes")
+    print("3. All quotes")
+    print("4. Add quote")
+    print("5. Exit")
 
 def main():
     while True:
@@ -16,11 +17,14 @@ def main():
         
         if choice == "1":
             print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
+        elif choice == "2" # gestion de display_count()
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
         elif choice == "3":
-          add_quote(quotes, "quotes.txt")
+            view_quotes(quotes)
         elif choice == "4":
+          add_quote(quotes, "quotes.txt")
+        elif choice == "5":
             print("Good bye...")
             break
         else:
