f873a0f8f71a000e558be362c0b67dfe1736d74f
ArthurD0 <131153831+ArthurD0@users.noreply.github.com>
2023-05-23 08:59:31 +0200

    Bug réglé

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
     print("2. All quotes")
-    print("3. Add quote)
+    print("3. Add quote")
     print("4. Exit")
 
 def main():
@@ -12,7 +12,7 @@ def main():
         quotes = load_quotes("quotes.txt")
         menu()
 
-        choice = input("Choose your an action (1-3): ")
+        choice = input("Choose your an action (1-4): ")
         
         if choice == "1":
             print_quote(random_quote(quotes))
